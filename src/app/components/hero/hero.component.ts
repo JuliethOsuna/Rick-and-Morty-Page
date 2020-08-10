fd2aee1cd6309b83cac168802fa8abfce1e80cd2
Julieth Osuna <santiago.gomez@bcdtravel.com.co>
2020-08-10 16:34:09 -0500

    developed logic in filter-component and init desktop styles

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+@Input()width;
+   
   constructor() { }
 
   ngOnInit(): void {
